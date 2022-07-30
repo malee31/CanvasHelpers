266019e206bc583f0d157e3806c6aef0776f3373
Marvin Lee <marvinlee.github@gmail.com>
2022-07-30 00:13:24 -0700

    Added rough Canvas logo

--- frontend/src/components/sections/Navbar.js
+++ frontend/src/components/sections/Navbar.js
@@ -1,9 +1,14 @@
 import "./Navbar.css";
+import RoughCanvasIcon from "../../static/rough_canvas.svg";
 
 export default function Navbar() {
 	return (
 		<nav className="nav-bar">
-			<span>CanvasHelpers UI</span>
+			<span style={{
+				display: "flex",
+				alignItems: "center",
+			}}>
+				<RoughCanvasIcon width="2rem" height="2rem"/> CanvasHelpers UI</span>
 		</nav>
 	);
 }
