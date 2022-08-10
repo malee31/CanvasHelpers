e04f67022074a00beaf36b7722926e3827d15fa0
Marvin Lee <marvinlee.github@gmail.com>
2022-08-09 17:36:36 -0700

    Show loading and error messages for course list

--- frontend/src/components/parts/Main/CourseCard.js
+++ frontend/src/components/parts/Main/CourseCard.js
@@ -9,6 +9,7 @@ export default function CourseCard() {
 	const course = global.data.course;
 	const [open, setOpen] = useState(true);
 	const [courses, setCourses] = useState(null);
+	const [errorMessage, setErrorMessage] = useState("Loading...");
 	const changeCourse = course => {
 		global.update({
 			course: {
@@ -26,9 +27,15 @@ export default function CourseCard() {
 				"Authorization": `Bearer ${API_KEY}`
 			}
 		} : undefined)
-			.then(res => res.json())
-			.then(newCourses => setCourses(newCourses))
-			.catch(console.error);
+			.then(async res => {
+				if(res.status === 200) {
+					const newCourses = await res.json();
+					setCourses(newCourses);
+					return;
+				}
+				setErrorMessage(`${res.status}: ${res.statusText} (Check API Key)`);
+			})
+			.catch(err => setErrorMessage(`${err.code}: ${err.message}`));
 	}, [API_KEY]);
 
 	return (
@@ -45,6 +52,9 @@ export default function CourseCard() {
 			</CardButton>
 			<div className={`course-card-select-wrapper ${open ? "course-card-select-wrapper-open" : ""}`}>
 				<div className="course-card-select">
+					{!Array.isArray(courses) && (
+						<p className="course-card-error-message">{errorMessage}</p>
+					)}
 					{Array.isArray(courses) && courses.map(course => (
 						<div
 							className="course-card-option"
